d9a1f981967530b0eab0e73790735b4d53fe66c7
VincentZyuwin11台式机 <VincentZyuwin11>
2025-12-06 01:39:21 +0800

    qwq

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema } from 'koishi'
 
 export const name = 'sky-blessing'
 
-export interface Config {}
+export interface Config {
+  backendUrl: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.intersect([
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  Schema.object({
+    backendUrl: Schema.string()
+      .default('http://192.168.31.84:51205')
+      .description('后端地址')
+  })
+
+])
+
+export function apply(ctx: Context, cfg: Config) {
+
+  ctx.command('光遇抽签')
+    .alias('awa_sky_blessing')
+    .alias('asb')
+    .action( async ( {session, options} ) => {
+
+      await session.send(`${h.quote(session.messageId)}${h.image(`${cfg.backendUrl}/blessing`)}`)
+
+    } )
+
 }
